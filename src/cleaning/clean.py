308c7c9f0720bf26ad237d63971e9d11a22041dd
Jason Joannou <jjoannou@bscglobal.com>
2024-05-20 22:37:25 +0200

    More methods to cleaning file
    The cleaning module now has more methods including:
    
    - removing duplicates
    - ensuring dates are datetime objects
    - new outlier removal method

--- src/cleaning/clean.py
+++ src/cleaning/clean.py
@@ -1,17 +1,65 @@
 import pandas as pd
+from .utility import count_outliers
+
 
 def fill_na_with_median_and_mode(df: pd.DataFrame) -> pd.DataFrame:
     # Fill NA in numeric columns with the median
-    for col in df.select_dtypes(include=['number']).columns:
+    for col in df.select_dtypes(include=["number"]).columns:
         median = df[col].median()
-        df[col].fillna(median, inplace=True)
+        df[col].fillna(median)
 
     # Fill NA in non-numeric columns with the mode
-    for col in df.select_dtypes(exclude=['number']).columns:
+    for col in df.select_dtypes(exclude=["number"]).columns:
         mode = df[col].mode()[0]
-        df[col].fillna(mode, inplace=True)
+        df[col].fillna(mode)
 
     return df
 
-def remove_outliers():
-    raise NotImplementedError("Not implemented yet")
+
+def convert_datetime_column(df: pd.DataFrame) -> pd.DataFrame:
+    df = pd.to_datetime(df["Date"])
+    return df
+
+
+def drop_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+    df.drop_duplicates(inplace=True)
+    return df
+
+
+def remove_outliers(
+    df: pd.DataFrame,
+    columns: list = ["High", "Low", "Open", "Close", "Volume", "Marketcap"],
+) -> pd.DataFrame:
+    cleaned_dfs = []
+
+    for coin_name, temp_df in df.groupby("Name"):
+        for col in columns:
+            Q1 = temp_df[col].quantile(0.25)
+            Q3 = temp_df[col].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            temp_df = temp_df[
+                (temp_df[col] >= lower_bound) & (temp_df[col] <= upper_bound)
+            ]
+
+        cleaned_dfs.append(temp_df)
+
+    return pd.concat(cleaned_dfs, ignore_index=True)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("./.data/coins.csv")
+    print(df.head())
+    print("----------")
+    print(count_outliers(df=df))
+    print("----------")
+    df = fill_na_with_median_and_mode(df=df)
+    print(df.head())
+    print("----------")
+    df = remove_outliers(df=df)
+    print(df.head())
+    print(len(df))
+    print("---------------")
+
+    print(count_outliers(df=df))
