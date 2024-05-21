cba7d2284043fd5b7637e9980cc1df0c916cc816
Jason Joannou <jjoannou@bscglobal.com>
2024-05-21 12:20:23 +0200

    Syntax changes

--- src/cleaning/read.py
+++ src/cleaning/read.py
@@ -4,14 +4,14 @@ from typing import List
 from .validation import validate_directory
 from .load import push_to_sqlite, write_to_csv
 
-def get_data_files(dir_path: str) -> List[str]:
 
+def get_data_files(dir_path: str) -> List[str]:
     try:
         validate_directory(dir_path=dir_path)
         files = []
         for file_name in os.listdir(dir_path):
             # Check if the file is a CSV
-            if file_name.endswith('.csv'):
+            if file_name.endswith(".csv"):
                 file_path = os.path.join(dir_path, file_name)
                 files.append(file_path)
 
@@ -22,24 +22,24 @@ def get_data_files(dir_path: str) -> List[str]:
         message = str(e)
         print(f"Main Error: {main_error}\nSub Error: {sub_error}\nMessage: {message}")
         return []
-    
+
+
 def concatenate_csv_files(files: List[str]) -> pd.DataFrame:
     data_frames = []
     sum_lengths = 0
     for file_path in files:
         df = pd.read_csv(file_path)
-        sum_lengths+= len(df)
+        sum_lengths += len(df)
         data_frames.append(df)
-    
+
     concatenated_df = pd.concat(data_frames, ignore_index=True)
     print(sum_lengths)
     print(len(concatenated_df))
     return concatenated_df
 
 
-
 if __name__ == "__main__":
     file_path = get_data_files(".data")
     df = concatenate_csv_files(files=file_path)
-    # write_to_csv(df=df)
-    push_to_sqlite(df=df, table_name="CoinsTable")
+    write_to_csv(df=df)
+    # push_to_sqlite(df=df, table_name="CoinsTable")
