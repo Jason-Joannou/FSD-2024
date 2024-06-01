66bea19d2a091bf3bdfc2479ca6341d8ad019326
Jason Joannou <jjoannou@bscglobal.com>
2024-06-01 10:46:22 +0200

    Removed histrogram and added summary plot

--- src/visualizations/plot_reporting.py
+++ src/visualizations/plot_reporting.py
@@ -3,26 +3,6 @@ import plotly.graph_objects as go
 from .utility import update_fig_layout
 import numpy as np
 
-def plot_histogram(df: pd.DataFrame, coin_name: str, x_column_name: str, num_bins: int = 50) -> go.Figure:
-    df = df[df["Name"] == coin_name]
-    density_line = np.histogram(df[x_column_name], bins=num_bins, density=True)
-    x_density, y_density = density_line[1], density_line[0]
-
-    median_line = np.median(df[x_column_name])
-
-    fig = go.Figure()
-    fig.add_trace(go.Histogram(x=df[x_column_name], histnorm='probability density', name='Histogram', nbinsx=num_bins))
-
-    fig.add_trace(go.Scatter(x=x_density, y=y_density, mode='lines', name='Density'))
-
-    fig.add_shape(type="line", x0=median_line, y0=0, x1=median_line, y1=max(y_density),
-                  line=dict(color="red", width=2, dash="dot"), name="Median")
-
-    fig = update_fig_layout(fig=fig, title=f"Histogram of {x_column_name} for {coin_name}",
-                            xaxis_title=x_column_name, yaxis_title="Density")
-
-    return fig
-
 def plot_boxplots(df: pd.DataFrame, coin_name: str) -> go.Figure:
     df = df[df["Name"] == coin_name]
     numeric_cols = df.select_dtypes(include=['number']).columns
@@ -30,6 +10,14 @@ def plot_boxplots(df: pd.DataFrame, coin_name: str) -> go.Figure:
     for col in numeric_cols:
         fig.add_trace(go.Box(y=df[col], name=col))
     fig = update_fig_layout(fig=fig, title=f"Box plots of numeric columns for {coin_name}", xaxis_title="Fields", yaxis_title="Values")
+    return fig
+
+def plot_summary_table(summary_df: pd.DataFrame) -> go.Figure:
+    fig = go.Figure()
+    fig.add_trace(go.Table(
+    header=dict(values=list(summary_df.columns), fill_color='lightblue', align='center'),
+    cells=dict(values=[summary_df[col] for col in summary_df.columns], fill_color='lightcyan', align='center')))
+
     return fig
 
 def plot_piechart(coin_counts: pd.DataFrame) -> go.Figure:
@@ -55,10 +43,8 @@ def plot_barchart(date_ranges: pd.DataFrame) -> go.Figure:
     fig = update_fig_layout(fig=fig, title="Date range for each coin", xaxis_title="Coin", yaxis_title="Date range (days)", showlegend=False)
     return fig
 
-def plot_switch(graph_type: str, coin_name: str, df: pd.DataFrame, x_column_name: str = "Close") -> go.Figure:
-    if graph_type == "histogram":
-        fig = plot_histogram(df=df, coin_name=coin_name, x_column_name=x_column_name)
-    elif graph_type == "boxplot":
+def plot_switch(graph_type: str, coin_name: str, df: pd.DataFrame) -> go.Figure:
+    if graph_type == "boxplot":
         fig = plot_boxplots(df=df, coin_name=coin_name)
     else:
         fig = go.Figure()
