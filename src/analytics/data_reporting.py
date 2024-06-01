cfdb03760d4abe9a55c694200e44bdc991dc9871
Jason Joannou <jjoannou@bscglobal.com>
2024-06-01 10:30:55 +0200

    Added a data reporting file to our analytics
    - This module is to calculate dataframe related analytics ie number of rows per coin, percentage of coins that make up the dataset etc.

---
+++ src/analytics/data_reporting.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def coin_proportion(df: pd.DataFrame) -> pd.DataFrame:
+    coin_proportion = df['Name'].value_counts(normalize=True) * 100
+    return coin_proportion
+
+def date_range_coins(df: pd.DataFrame) -> pd.DataFrame:
+    date_ranges = df.groupby('Name')['Date'].agg(['min', 'max'])
+    return date_ranges
+
+def records_per_coin(df: pd.DataFrame):
+    coin_counts = df['Name'].value_counts()
+    return coin_counts
+
+def coin_summary_info(df: pd.DataFrame):
+    summary_info = df.groupby('Name').agg({
+        'Date': ['min', 'max', 'count'],
+        'Volume': 'sum',
+        'Marketcap': 'mean'
+    })
+    summary_info.columns = ['Start Date', 'End Date', 'Number of Records', 'Total Volume', 'Average Market Cap']
+    return summary_info
+
+
