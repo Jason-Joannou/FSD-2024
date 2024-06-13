344056aa3903f78e9ab849e0e6af55f844bf4252
Jason Joannou <jjoannou@bscglobal.com>
2024-06-14 01:00:41 +0200

    Added some calculations and processesing to the machine learning module

--- ml/feature_engineering.py
+++ ml/feature_engineering.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import numpy as np
+from .utility import (calculate_sma,
+                      calculate_ema,
+                      calculate_macd,
+                      calculate_bollinger_bands,
+                      calculate_rsi, 
+                      calculate_volume_oscillator,
+                      calculate_roc,
+                      calculate_atr,
+                      calculate_lag_features)
 
-def date_features(df: pd.DataFrame) -> pd.DataFrame:
+
+def add_date_features(df: pd.DataFrame) -> pd.DataFrame:
     df['Date'] = pd.to_datetime(df['Date'])
     df['Day'] = df['Date'].dt.day
     df['Month'] = df['Date'].dt.month
@@ -11,33 +21,31 @@ def date_features(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def price_features(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.sort_values(["Name", "Date"])
-    df['ClosingPriceChange'] = df.groupby('Name')['Close'].pct_change().fillna(0)
-    df['OpeningPriceChange'] = df.groupby('Name')['Open'].pct_change().fillna(0)
+def add_technical_features(df: pd.DataFrame) -> pd.DataFrame:
+    df = calculate_lag_features(df=df, lags=2)
+    df["SMA_10"] = calculate_sma(data=df["Close"], window=10)
+    df["SMA_50"] = calculate_sma(data=df["Close"], window=50)
+    df["EMA_10"] = calculate_ema(data=df["Close"], span=10)
+    df["EMA_50"] = calculate_ema(data=df["Close"], span=50)
+    df['RSI'] = calculate_rsi(df['Close'])
+    df['MACD'] = calculate_macd(df['Close'])
+    df['Bollinger_High'], df['Bollinger_Low'] = calculate_bollinger_bands(df['Close'])
+    df['Volume_Oscillator'] = calculate_volume_oscillator(df['Volume'])
+    df['ROC'] = calculate_roc(df['Close'])
+    df['ATR'] = calculate_atr(df['High'], df['Low'], df['Close'])
 
     return df
 
-def volatility_features(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.sort_values(["Name", "Date"])
-    df['PriceRange'] = df['High'] - df['Low']
-    df['PriceRangePercentage'] = (df['PriceRange'] / df['Low']) * 100
-    return df
 
-def moving_averages(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.sort_values(["Name", "Date"])
-    df['Closing_MA_5'] = df['Close'].rolling(window=5).mean()
-    df['Closing_MA_10'] = df['Close'].rolling(window=10).mean()
-    df['Volume_MA_5'] = df['Volume'].rolling(window=5).mean()
-    df['Volume_MA_10'] = df['Volume'].rolling(window=10).mean()
-    return df
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = add_date_features(df=df)
+    df = add_technical_features(df=df)
 
+    return df
 
 if __name__ == "__main__":
     df = pd.read_csv("./.data/coins.csv")
-    df = date_features(df=df)
-    df = price_features(df=df)
-    df = volatility_features(df=df)
-    print(df.head(20))
+    df = preprocess_data(df=df)
+    
 
 
--- ml/utility.py
+++ ml/utility.py
@@ -1,5 +1,63 @@
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from typing import Tuple
 
 def split_data(features, target, split=0.2):
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=split)
     return X_train, X_test, y_train, y_test
+
+def calculate_sma(data: pd.Series, window: int = 5) -> pd.Series:
+    data = data.rolling(window=window).mean()
+    return data
+
+def calculate_ema(data: pd.Series, span: int = 10) -> pd.Series:
+    data = data.ewm(span=span, adjust=False)
+    return data
+
+def calculate_rsi(series: pd.Series, period: int = 14) -> pd.Series:
+    delta = series.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def calculate_macd(series: pd.Series, span1: int = 12, span2: int = 26) -> pd.Series:
+    exp1 = series.ewm(span=span1, adjust=False).mean()
+    exp2 = series.ewm(span=span2, adjust=False).mean()
+    macd = exp1 - exp2
+    return macd
+
+def calculate_bollinger_bands(series: pd.Series, window: int = 20) -> Tuple[pd.Series, pd.Series]:
+    sma = series.rolling(window=window).mean()
+    std = series.rolling(window=window).std()
+    bollinger_high = sma + (std * 2)
+    bollinger_low = sma - (std * 2)
+    return bollinger_high, bollinger_low
+
+def calculate_volume_oscillator(volume: pd.Series, short_window: int = 12, long_window: int = 26) -> pd.Series:
+    short_volume_ma = volume.rolling(window=short_window).mean()
+    long_volume_ma = volume.rolling(window=long_window).mean()
+    volume_oscillator = (short_volume_ma - long_volume_ma) / long_volume_ma
+    return volume_oscillator
+
+def calculate_roc(series: pd.Series, period: int = 12) -> pd.Series:
+    roc = ((series - series.shift(period)) / series.shift(period)) * 100
+    return roc
+
+def calculate_atr(high: pd.Series, low: pd.Series, close: pd.Series, window: int = 14) -> pd.Series:
+    tr = pd.DataFrame()
+    tr['TR1'] = abs(high - low)
+    tr['TR2'] = abs(high - close.shift())
+    tr['TR3'] = abs(low - close.shift())
+    true_range = tr.max(axis=1)
+    atr = true_range.rolling(window=window).mean()
+    return atr
+
+def calculate_lag_features(df: pd.DataFrame, lags: int = 1) -> pd.DataFrame:
+    for lag in range(1, lags + 1):
+        df[f'Lag_{lag}_Close'] = df['Close'].shift(lag)
+        df[f'Lag_{lag}_High'] = df['High'].shift(lag)
+        df[f'Lag_{lag}_Low'] = df['Low'].shift(lag)
+        df[f'Lag_{lag}_Volume'] = df['Volume'].shift(lag)
+    return df
