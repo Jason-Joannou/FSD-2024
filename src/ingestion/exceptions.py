d48663cac98a0003cc02420567eab29f0cf94ec0
Jason Joannou <jjoannou@bscglobal.com>
2024-06-13 23:50:52 +0200

    Added docstring to ingestion modules

--- src/ingestion/exceptions.py
+++ src/ingestion/exceptions.py
@@ -1,3 +1,20 @@
 class InvalidDatasetName(Exception):
+    """
+    Exception raised for invalid dataset names.
+
+    Attributes:
+        message (str): Explanation of the error.
+    
+    Methods:
+        __init__(self, message: str) -> None:
+            Initializes the exception with a message.
+    """
+    
     def __init__(self, message: str) -> None:
+        """
+        Initialize the InvalidDatasetName exception.
+
+        Parameters:
+            message (str): Explanation of the error.
+        """
         super().__init__(message)
--- src/ingestion/validation.py
+++ src/ingestion/validation.py
@@ -1,6 +1,18 @@
 from .exceptions import InvalidDatasetName
 
 def validate_dataset_name(dataset_name: str) -> None:
+    """
+    Validates the format of a Kaggle dataset name.
+
+    Args:
+        dataset_name (str): The name of the dataset to validate. The format should be '[owner]/[dataset]'.
+
+    Raises:
+        InvalidDatasetName: If the dataset name is not in the correct format, contains spaces in the owner or dataset part, or has more than one '/'.
+
+    Example:
+        validate_dataset_name("owner/dataset")
+    """
     validation_array = dataset_name.split("/")
     
     # Check correct format
@@ -18,12 +30,6 @@ def validate_dataset_name(dataset_name: str) -> None:
         message = f"The dataset name '{validation_array[1]}' contains spaces. Dataset name should not contain spaces."
         raise InvalidDatasetName(message)
     
-    if len(validation_array)>2:
+    if len(validation_array) > 2:
         message = f"The dataset name '{dataset_name}' is not in the right format. The format should be '[owner]/[dataset]'."
         raise InvalidDatasetName(message)
-
-
-    
-
-    
-    
