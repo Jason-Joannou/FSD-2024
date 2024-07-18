7e832fabe50b9fdd24fc7116dad25698a531ca80
Jason Joannou <jjoannou@bscglobal.com>
2024-07-18 14:22:51 +0200

    New validation methods for updating user info

--- api/validation.py
+++ api/validation.py
@@ -1,10 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
-    email: str
+    email: EmailStr
     password: str
 
 class UserLogin(BaseModel):
     username: str
     password: str
+
+class UsernameUpdate(BaseModel):
+    old_username: str
+    new_username: str
+
+class EmailUpdate(BaseModel):
+    username: str
+    new_email: str
+
+class PasswordUpdate(BaseModel):
+    username: str
+    current_password: str
+    new_password: str
+    confirm_password: str
+
